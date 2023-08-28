1339f3e540f6484b8d9e74aaa1a1d1cb6d758b81
franciscoMarquezBocanegra <francisco.marquez@mail.escuelaing.edu.co>
2023-08-27 21:51:38 -0500

    entrega taller

---
+++ src/main/resources/static/files/prueba.js
@@ -0,0 +1,5 @@
+
+function calcularAreaCirculo(radio) {
+  var area = Math.PI * Math.pow(radio, 2);
+  return area;
+}
---
+++ target/classes/static/js/search.js
@@ -0,0 +1,15 @@
+function openImage() {
+    window.location.href = "/files/imagen.png";
+}
+
+function openHtml() {
+    window.location.href = "/files/hola.html";
+}
+
+function openJs() {
+    window.location.href = "/files/prueba.js";
+}
+
+function openCss() {
+    window.location.href = "/files/prueba.css";
+}
